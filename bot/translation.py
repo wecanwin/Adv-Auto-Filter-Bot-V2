3a9f5ebabdf2b5b3edecc5d00c31b65e8e93d216
Hemas0308 <81526699+Hemas0308@users.noreply.github.com>
2021-06-18 16:29:10 +0400

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,46 +4,55 @@
 
 class Translation(object):
     
-   START_MSG = """ <b>Hi {}
+    START_TEXT = """<b>Hey {}!!</b>
+<i>ஏடகம்-Library ஒரு டிஜிட்டல் காப்பகமும் டிஜிட்டல் நூலகமும் ஆகும்....😉
 
-ஏடகம்-Library ஒரு டிஜிட்டல் காப்பகமும் டிஜிட்டல் நூலகமும் ஆகும்.
+டெலிகிராம் குழுமங்களில் மிகவும் தரமான நூல்களை வழங்கும் முதன்மையான ஒரே நூலகம்.! 🔥🔥😝
 
-
-
-டெலிகிராம் குழுமங்களில் மிகவும் தரமான !
-
-நூல்களை வழங்கும் முதன்மையான ஒரே நூலகம்.!
-
-For more click <i>help</i></b>"""
-
-
-    HELP_MSG = """<b>How to use the bot??</b>
-<i>
-* Add bot to your group with admin rights.
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-<b>© @librarytamil</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>https://telegram.me/aedahamlibrary_noolakam</b>
+For More Details Click Help Button Below..
+@bookflowerss
+</i>"""    
     
-⭕️<b>Creater :</b> @kaimudinjavai
-⭕️<b>Language :</b> <code>Python3</code>
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+    HELP_TEXT = """
+<b><i><u>How To Use Me!?</u></i></b>
 
+<i>
+-> Add Me To Any Group And Make Me Admin
+-> Add Me To Your Desired Channel
+</i>
+
+<b>Bot Commands (Works Only In Groups) :</b>
+
+    -> <code>/add chat_id</code>
+                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
+     <code>/add @Username</code>
+     
+    -> <code>/del chat_id</code>
+                OR                  - To disconnect A Group With A Channel
+     <code>/del @Username</code>
+     
+    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    
+    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+
+            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+            
+            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+
+            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            
+            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            
+@bookflowerss
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    
+<b>➥ Creator</b> : <b><i><a href="https://t.me/kaimudinjavai">kaimudinjavai</a></i></b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Source Code</b> : <i><a href="https://github.com/Hemas0308/Adv-Auto-Filter-Bot-V2">Click Me</a></i>
 """
